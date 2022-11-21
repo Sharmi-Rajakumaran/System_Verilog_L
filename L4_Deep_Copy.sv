6477233ff122c9fd65fd95bccee34851bff015e2
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 12:54:03 +0530

    Update L4_Deep_Copy.sv

--- L4_Deep_Copy.sv
+++ L4_Deep_Copy.sv
@@ -1,78 +1,83 @@
 module test_deep_copy;
-
-
-        // In class parity_calc_c
-
         class parity_calc_c;
-
-                // Declare parity (bit type, size 8), initialize it with some random value
-                  bit [7:0] parity = 40;
-                // Write copy method that returns parity_calc_c class type
+                bit [7:0] parity = 30; // creating a property and allocating value
+                
                 function parity_calc_c copy();
-                        // Create copy instance
-                        copy = new;
-                        // Copy all the current properties into copy object
-                                copy.parity = this.parity;
+                        copy = new;   
+                        copy.parity = this.parity;
                 endfunction: copy
-
+                
+                  // function copy is created with return type as class data type parity_calc_c
+                  // copy object created in the function 
+                  // The copy object contains the properties of parity_calc_c class. The property of the class parity_calc_c is assigned to the copy object
+       
         endclass: parity_calc_c
-
-        // In class packet_c
-
+        
         class packet_c;
-
-                // Declare header (bit type , size 8), initialize it with some random value
-                 bit [7:0] header = 30;
-                // Declare data (bit type , size 8), initialize it with some random value
-                bit [7:0]data = 56;
-                // Declare and create an instance of parity_calc_c
-            parity_calc_c par_h = new;
-                // Define copy method that returns packet_c class type
-             function packet_c copy();
-                        // Create copy instance
+                bit [7:0]header = 20;
+                bit [7:0]data = 40;
+                
+                parity_calc_c par_h = new;      // an object of parity class is created in packet class with handle par_h
+                
+                function packet_c copy();
                         copy = new();
-                        // Copy all the current class properties into copy object
-                                copy.header = this.header;
-               copy.data = this.data;
-                                copy.par_h = this.par_h.copy();
-            endfunction: copy
+                        copy.header = this.header;
+                        copy.data = this.data;
+                        copy.par_h = this.par_h.copy();
+                endfunction: packet_c
+                
         endclass: packet_c
-    // Declare 2 handles pkt_h1 & pkt_h2 for packet_c class 
+        
         packet_c pkt_h1;
         packet_c pkt_h2;
-
-        // Within initial
-initial
-        begin
-                // Create pkt_h1 object
-        pkt_h1 = new;
-                // Use shallow copy method to copy pkt_h1 to pkt_h2 
-        pkt_h2 = new pkt_h1;
-                // Display the properties of parent class and sub class properties of pkt_h1 and pkt_h2
-        $display("PERFORMING SHALLOW COPY");
-        $display("pkt_h1 after shallow copy {Header = %d, Data = %d, parity = %d}", );
-                // Assign random value to the header of pkt_h2
-
-                // Display the properties of parent class and sub-class properties of pkt_h1 and pkt_h2
-                // observe pkt_h1.header does not change
-
-                // Change parity of pkt_h2 using subclass handle from the parent class packet_c
-                // Ex: pkt_h2.par.parity=19;
-
-                // Display the properties of parent class and sub-class properties of pkt_h1 and pkt_h2
-                // observe that change reflected in pkt_h1 as the subclass handle in pkt_h1 and pkt_h2 are pointing to same subclass object
-
-                // Perform deep copy by calling parent class copy method
-                // Ex: pkt_h2=pkt_h1.copy;
-
-                // Display the properties of parent class and sub-class properties of pkt_h1 and pkt_h2
-                // observe the parent and subclass properties
-
-                // Change parity of pkt_h2
-                // Ex: pkt_h2.par.parity=210;
-
-                // Display the properties of parent class and sub-class properties of pkt_h1 and pkt_h2
-                // observe that parity doesnot change for pkt_h1 as they are two different subclass objects
-
-
-endmodule
+        
+        
+        initial
+                begin
+                        pkt_h1 = new;
+                         // copy pkt_h1 to pkt_h2 using shallow copy method
+                        pkt_h2 = new pkt_h1;
+                        
+                        $display("Performing Shallow Copy");
+                        $display("Pkt_h1 after shallow copy {Header = %d, Data = %d, Parity = %d}", pkt_h1.header, pkt_h1.data, pkt_h1.par_h.parity);
+                        $display("Pkt_h2 after shallow copy {Header = %d, Data = %d, Parity = %d}", pkt_h2.header, pkt_h2.data, pkt_h2.par_h.parity);
+                        
+                        pkt_h2.header = 50; // Random value is assigned to the Header, this is only for pkt_h2
+                        
+                        $display("CHANGING THE VALUE OF pkt_h2: pkt_h1 - header is not changes. Pkt_h1 and pkt_h2 are two different copies of packet_c objects");
+                        $display("Pkt_h1 after updating header {Header = %d, Data = %d, Parity = %d}", pkt_h1.header, pkt_h1.data, pkt_h1.par_h.parity);
+                        $display("Pkt_h2 after updating header {Header = %d, Data = %d, Parity = %d}", pkt_h2.header, pkt_h2.data, pkt_h2.par_h.parity);
+                        
+                        pkt_h2.par_h.parity = 19;
+                        
+                        $display("CHANGING THE VALUE OF pkt_h2: parity after shallow copy - parity of both pkt_h1 and pkt_h2 are changed since both copies are pointing to the same parity_calc_c object which implies shallow copy");
+                        $display("Pkt_h1 after updating parity {Header = %d, Data = %d, Parity = %d}", pkt_h1.header, pkt_h1.data, pkt_h1.par_h.parity);
+                        $display("Pkt_h2 after updating parity {Header = %d, Data = %d, Parity = %d}", pkt_h2.header, pkt_h2.data, pkt_h2.par_h.parity);
+                        
+                        
+                        
+                        
+                        // Performing Deep Copy after calling parent class copy method
+                        
+                        pkt_h2 = pkt_h1.copy();
+                        
+                        // The properties of parent class and subclass properties of pkt_h1 and pkt_h2 are displayed
+                        
+                        $display("Performing Deep Copy");
+                        $display("Pkt_h1 after deep copy {Header = %d, Data = %d, Parity = %d}", pkt_h1.header, pkt_h1.data, pkt_h1.par_h.parity);
+                        $display("Pkt_h2 after deep copy {Header = %d, Data = %d, Parity = %d}", pkt_h2.header, pkt_h2.data, pkt_h2.par_h.parity);
+                        
+                     
+                        pkt_h2.par_h.parity = 500;
+                        
+                        
+                        $display("Displaying the properties of parent class and subclass properties of pkt_h1 and pkt_h2);
+                        $display("Pkt_h1 after updating parity{Header = %d, Data = %d, Parity = %d}", pkt_h1.header, pkt_h1.data, pkt_h1.par_h.parity);
+                        $display("Pkt_h2 after updating parity{Header = %d, Data = %d, Parity = %d}", pkt_h2.header, pkt_h2.data, pkt_h2.par_h.parity);
+                        
+                     
+                
+               end
+       endmodule
+                                 
+                
