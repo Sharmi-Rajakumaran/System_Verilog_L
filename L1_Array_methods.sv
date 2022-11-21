03c837140eed7cff3c598705c0b410076425d0d7
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 17:14:59 +0530

    Update L1_Array_methods.sv

--- L1_Array_methods.sv
+++ L1_Array_methods.sv
@@ -171,5 +171,104 @@ $display("The size of the associative array is %0p",data_mem.size);
 
 
 endmodule
+
+/*
+# Dynamic Array Elements of data_da[0] = 8
+# Dynamic Array Elements of data_da[1] = 17
+# Dynamic Array Elements of data_da[2] = 17
+# Dynamic Array Elements of data_da[3] = 7
+# Dynamic Array Elements of data_da[4] = 17
+# Dynamic Array Elements of data_da[5] = 17
+# Dynamic Array Elements of data_da[6] = 5
+# Dynamic Array Elements of data_da[7] = 2
+# Dynamic Array Elements of data_da[8] = 1
+# Dynamic Array Elements of data_da[9] = 9
+# 
+#  
+#  
+#  
+#  Array Reduction Methods -- sum, product, and, exor
+# Sum of Dynamic Array Elements is 100
+# product of Dynamic Array Elements is  420945840
+# And of Dynamic Array Elements is  0
+# Or of Dynamic Array Elements is  31
+# Xor of Dynamic Array Elements is  0
+# 
+#  
+#  
+#  
+#  Array Reduction Methods with "with" clause
+# Sum of Dynamic Array Elements with items greater than 7 is  6
+# Sum of Dynamic Array Elements with items equal to 8 is  1
+# Sum of Dynamic Array Elements with items greater than 7 is  0
+# Sum of Dynamic Array Elements with items equal to 7 is  0
+# Sum of Dynamic Array Elements with items greater than 7 is  0
+# Xor of Dynamic Array Elements with items equal to 7 is  1
+# Or of Dynamic Array Elements with items less than 7 is  1
+# Or of Dynamic Array Elements with items equal to 10 is  0
+# Or of Dynamic Array Elements with items equal to 10 is  0
+# Or of Dynamic Array Elements with items equal to 10 is  0
+# 
+# 
+# 
+#  Basic dynamic array operations
+# Size of the new array data_da is          10
+# Dynamic Array Elements  - (appending 30 new elements and including the data_da) array of data_da1[0] = 8
+# Size of the new array data_da1 is          30
+# Dynamic Array Elements of data_da[0] = 0
+# 
+# 
+# 
+#  Basic array sorting methods -- reverse, sort, rsort, shuffle
+#  Reversal of Dynamic Array Elements  '{9, 1, 2, 5, 17, 17, 7, 17, 17, 8}
+#  RAscending order of Dynamic Array Elements  '{1, 2, 5, 7, 8, 9, 17, 17, 17, 17}
+#  Descending order of Dynamic Array Elements  '{17, 17, 17, 17, 9, 8, 7, 5, 2, 1}
+#  Shuffling of Dynamic Array Elements  '{1, 17, 5, 7, 17, 8, 17, 9, 2, 17}
+# 
+# 
+# 
+#  Basic array Locator methods -- min, max, unique, unique_index, find, find_index, find_first, find_first_index, find_last, find_last_index
+#  Minimum of Dynamic Array Elements '{1}
+#  Maximum order of Dynamic Array Elements '{17}
+#  Unique elements of Dynamic Array Elements '{1, 2, 5, 7, 8, 9, 17}
+#  Find first item  >5  in Dynamic Array Elements '{17}
+#  Find first item  >5  in Dynamic Array Elements '{1}
+#  Find first item  >5  in Dynamic Array Elements '{17}
+#  Find first item  >5  in Dynamic Array Elements '{1}
+# The elements of the queue are 18
+# The elements of the queue are 97
+# The elements of the queue are 29
+# The elements of the queue are 12
+# The elements of the queue are 89
+# The elements of the queue are 26
+# The elements of the queue are 97
+# The elements of the queue are 22
+# The elements of the queue are 85
+# The elements of the queue are 55
+# The elements of the queue are 0
+# 
+# 
+# 
+# Update the associative array with random data less than 200 based on the address stored in the queue - Get the address using pop method
+# data_mem[12]        150
+# data_mem[18]        194
+# data_mem[22]         93
+# data_mem[26]         53
+# data_mem[29]        186
+# data_mem[55]        189
+# data_mem[85]         29
+# data_mem[89]        168
+# data_mem[97]         84
+# 
+# 
+# 
+#  Dispay the contents of associative array using foreach loop
+# The first index of the associative array is 12
+# The element of first index of the associative array is 150
+# The first index of the associative array is 97
+# The element of first index of the associative array  is 84
+# The number of elements in the  associative array is 9
+# The size of the associative array is 9
+*/
                                                                                                                                                                                           72,1          47%
 
