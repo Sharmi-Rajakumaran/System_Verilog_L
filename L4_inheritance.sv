901b34c37d7b3940d2b4d397113f319ae5973872
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 16:15:33 +0530

    Update L4_inheritance.sv

--- L4_inheritance.sv
+++ L4_inheritance.sv
@@ -1,53 +1,46 @@
 
 module test_inheritance;
 
-        // In class account_base;
+       
         class account_base;
-                // Declare property balance int type and initialize with value 2000
+               
+                int balance = 2000;
 
                 // In the function summary_base ,return balance
+                function int summary_base();
+                        return balance;
+                endfunction: summary_base
 
-
-
-
-                // In the task deposit, 
-                // Pass an input argument 'pay' of int type 
-                // Add pay with previous balance
+                
+                task deposit(input int pay);
+                        balance = balance + pay;
+                endtask: deposit
 
         endclass: account_base
 
         // Extend class account_extd from account_base;
-
-
         class account_extd extends account_base;
-                // Declare property balance of int type and initialize with value 3000
-
-
-
-                // In function summary_extd, return balance from extended class
-                // the new value returned should be summation of base class balance and extended class balance 
-                // Hint: use super to access the base class balance
+                int balance = 3000;
+                
+                function int summary_extd();
+                        balance = super.balance + balance;
+                        return balance;
+                endfunction: summay_extd
 
         endclass: account_extd
 
-        // Declare a handle for account_extd class as acc_h
+    
+        account_extd acc_h;
 
-        // Within initial block,
+   initial
+           begin
+                   acc_h = new;
 
-                        // Create an object for acc_h
-
-                        // Pass the amount for the method deposit and 
-                        // Call the method summary_base to display the base class balance
-
-
-
-
-
-                        // Call method summary_extd to display balance
-                        // Observe that the super.balance returns the base class balance
-
-
-
-
+                   acc_h.deposit(8000);
+                   $display("Total balance now is %0d", acc_h.summay_base());
+                        
+                   $display("Total balance in the extended and base class together is %d", acc_h.summary_extd);
+           end
+ 
 endmodule : test_inheritance
                                       
