c870525eb009593ca42e0a63081656029ec1692b
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-21 17:12:05 +0530

    Update L4_Polymorphism.sv

--- L4_Polymorphism.sv
+++ L4_Polymorphism.sv
@@ -7,22 +7,39 @@ module test_polymorphism;
                         $display("Sending Base Class Packet");
                 endtask: send
 
-
-                class badpacket_c extends packet_c
-                        
+        endclass: packet_c
+                class badpacket_c extends packet_c;
+    
                         task send();
                                 $display("Sending Derived class packet");
                         endtask: send
-        initial
+       endclass: badpacket_c
+
+         initial
                 begin
                         badpacket_c bp_h =new;
                         packet_c pkt_h =new;
-                        
+    
                         pkt_h.send();
                         bp_h.send();
-                        
+    
                         pkt_h = bp_h;
                         pkt_h.send();
-                end
+                end 
 endmodule
+
+/* Simulation Result with virtual keyword in parent class
+# Sending Base Class Packet
+# Sending Derived class packet
+# Sending Derived class packet
+
+Simulation Result with virtual keyword in parent class
+# Sending Base Class Packet
+# Sending Derived class packet
+# Sending Base Class Packet
+
+*/
+
+
+
 
