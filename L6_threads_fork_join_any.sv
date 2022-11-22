bc0e26980a0a6da63902a4ee2f9641ab82ef0da1
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-22 19:06:53 +0530

    Update L6_threads_fork_join_any.sv

--- L6_threads_fork_join_any.sv
+++ L6_threads_fork_join_any.sv
@@ -1,12 +1,13 @@
 module test_threads;
   initial
+
     begin
-      $display("Thread_1 with dealy @ %0t" $time);
+      $display("Thread_1 with delay @ %0t", $time);
       #20 $display("Thread_2 with delay @ %0t", $time);
         fork: A
         // Thread A1 will be started at time t = 20
-          #40 $display("Thread with delay @%t", $time); // longest thread
-          
+          #40 $display("Thread A1 with delay @%t",$time); // longest thread
+              
           begin: B // Thread B with a delay of 15
             #5 $display("Thread B1 with delay: @ %0t", $time);
             #10 $display("Thread B2 with dealy: @ %0t", $time);
@@ -14,7 +15,33 @@ module test_threads;
           #5 $display("Thread_3 with delay: @ %0t", $time);
           // Shortest thread
         join_any
+      
+      // disable A;
       $display("Completed for thread A: @%0t", $time);
       #30 $display("Thread_4 with delay: @%0t", $time);
-    end
+    end 
 endmodule
+
+
+/*
+
+Output of fork join_any without disable 
+
+# Thread_1 with delay @ 0
+# Thread_2 with delay @ 20
+# Thread B1 with delay: @ 25
+# Thread_3 with delay: @ 25
+# Completed for thread A: @25
+# Thread B2 with dealy: @ 35
+# Thread_4 with delay: @ 55
+# Thread A1 with delay @ 60
+
+Output of fork join_any with disable 
+# Thread_1 with delay @ 0
+# Thread_2 with delay @ 20
+# Thread B1 with delay: @ 25
+# Thread_3 with delay: @ 25
+# Completed for thread A: @25
+# Thread_4 with delay: @55
+
+*/
