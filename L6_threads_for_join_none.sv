323e8363ad27246fd28dfdd849c83476fd8eccee
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-22 19:11:13 +0530

    Update L6_threads_for_join_none.sv

--- L6_threads_for_join_none.sv
+++ L6_threads_for_join_none.sv
@@ -14,7 +14,27 @@ module test_threads;
           #5 $display("Thread_3 with delay: @ %0t", $time);
           // Shortest thread
         join_none
+      // disable A;
       $display("Completed for thread A: @%0t", $time);
       #30 $display("Thread_4 with delay: @%0t", $time);
     end
 endmodule
+
+/*
+Output of Fork Join None Block without disabling block A
+# Thread_1 with dealy @ 0
+# Thread_2 with delay @ 20
+# Completed for thread A: @20
+# Thread B1 with delay: @ 25
+# Thread_3 with delay: @ 25
+# Thread B2 with dealy: @ 35
+# Thread_4 with delay: @50
+# Thread with delay:  @60
+
+Output of Fork Join None Block with disable A
+# Thread_1 with dealy @ 0
+# Thread_2 with delay @ 20
+# Completed for thread A: @20
+# Thread_4 with delay: @50
+
+*/
